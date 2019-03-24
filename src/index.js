4bf436c93cd7fa5d4551e0b2f0ebeab147bcdb04
kacper.sledz <casper.sledx@gmail.com>
2019-03-24 22:52:21 +0100

    start with create-react-app
    fix minor errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import '../node_modules/pokemon-font/css/pokemon-font.css'
-import '../style/main.sass'
+import '../node_modules/pokemon-font/css/pokemon-font.css';
+import './style/main.scss';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 // import Font from './components/Font'
-import Game from './containers/Game'
+import Game from './containers/Game';
+import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<Font />, document.getElementById('font'))
-ReactDOM.render(<Game />, document.getElementById('pokemon'))
+ReactDOM.render(<Game />, document.getElementById('pokemon'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
